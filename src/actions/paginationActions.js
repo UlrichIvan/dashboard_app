0b9bb3d62ffc17d1882cf88a0095d99142aa1267
UlrichIvan <mtchokos@gmail.com>
2023-10-17 13:57:57 +0100

    Fixed all rows selection for each page

--- src/actions/paginationActions.js
+++ src/actions/paginationActions.js
@@ -1,347 +1,327 @@
 import { DEFAULT_VIEW, REDUX_ACTIONS, rowsOptions } from "../constants";
-import { getIndexes, getPages, getRowsPeerPages } from "../reducers/paginationReducer";
+import {
+  getIndexes,
+  getPages,
+  getRowsPeerPages,
+} from "../reducers/paginationReducer";
 
 export const setHeaders = (payload) => {
-    return {
-        type: REDUX_ACTIONS.SET_HEADERS,
-        payload
-    }
-}
-
+  return {
+    type: REDUX_ACTIONS.SET_HEADERS,
+    payload,
+  };
+};
 
 export const setRows = (payload) => {
+  let rows = {};
 
-    let rows = {}
+  // init rows
+  for (const options of rowsOptions) {
+    let { value: period } = options;
 
-    // init rows
-    for (const options of rowsOptions) {
+    period = parseInt(period, 10);
 
-        let { value: period } = options
+    rows[period] = {
+      page: 1,
+      rowsPages: [],
+      period,
+      pages: getPages(payload, period), // get pages for each period
+    };
 
-        period = parseInt(period, 10)
+    let pages = rows[period].pages;
 
-        rows[period] = {
-            page: 1,
-            rowsPages: [],
-            period,
-            pages: getPages(payload, period),// get pages for each period
-        }
+    // get rows for each page index associate to specific period
+    for (let page = 1; page <= pages; page++) {
+      let rowsPage = getRowsPeerPages(payload, DEFAULT_VIEW, page, period)?.map(
+        (row) => ({
+          row,
+          checked: false,
+        })
+      );
 
-
-        let pages = rows[period].pages
-
-        // get rows for each page index associate to specific period
-        for (let page = 1; page <= pages; page++) {
-
-            let rowsPage = getRowsPeerPages(payload, DEFAULT_VIEW, page, period)?.map((row) => ({
-                row,
-                checked: false
-            }))
-
-            rows[period].rowsPages.push({
-                [page]: rowsPage,
-                views: getIndexes(payload.length, DEFAULT_VIEW, page, period),
-                allSelection: false
-            })
-        }
-
+      rows[period].rowsPages.push({
+        [page]: rowsPage,
+        views: getIndexes(payload.length, DEFAULT_VIEW, page, period),
+        allSelection: false,
+      });
     }
+  }
 
-    // console.log({ rows })
+  // console.log({ rows })
 
-    return {
-        type: REDUX_ACTIONS.SET_ROWS,
-        payload: rows
-    }
-}
+  return {
+    type: REDUX_ACTIONS.SET_ROWS,
+    payload: rows,
+  };
+};
 
 export const setUrl = (payload) => {
-    return {
-        type: REDUX_ACTIONS.SET_URL,
-        payload
-    }
-}
+  return {
+    type: REDUX_ACTIONS.SET_URL,
+    payload,
+  };
+};
 
 export const setPeriod = (payload) => {
-    return {
-        type: REDUX_ACTIONS.SET_PERIOD,
-        payload
-    }
-}
+  return {
+    type: REDUX_ACTIONS.SET_PERIOD,
+    payload,
+  };
+};
 
 export const setViews = (payload) => {
-    return {
-        type: REDUX_ACTIONS.SET_VIEWS,
-        payload
-    }
-}
+  return {
+    type: REDUX_ACTIONS.SET_VIEWS,
+    payload,
+  };
+};
 
 export const toggleOverlay = (payload) => {
-    return {
-        type: REDUX_ACTIONS.TOGGLE_OVERLAY,
-        payload
-    }
-}
+  return {
+    type: REDUX_ACTIONS.TOGGLE_OVERLAY,
+    payload,
+  };
+};
 
 export const setPages = (payload) => {
-    return {
-        type: REDUX_ACTIONS.SET_PAGES,
-        payload
-    }
-}
+  return {
+    type: REDUX_ACTIONS.SET_PAGES,
+    payload,
+  };
+};
 
 export const setPage = (payload) => {
-    return {
-        type: REDUX_ACTIONS.SET_PAGE,
-        payload
-    }
-}
-
+  return {
+    type: REDUX_ACTIONS.SET_PAGE,
+    payload,
+  };
+};
 
 export const setRowsPeerPages = (payload) => {
-    return {
-        type: REDUX_ACTIONS.SET_ROWS_PEER_PAGES,
-        payload
-    }
-}
+  return {
+    type: REDUX_ACTIONS.SET_ROWS_PEER_PAGES,
+    payload,
+  };
+};
 
 export const setInitRowsPeerPages = () => {
-    return {
-        type: REDUX_ACTIONS.INIT_ROWS_PEER_PAGES,
-    }
-}
+  return {
+    type: REDUX_ACTIONS.INIT_ROWS_PEER_PAGES,
+  };
+};
 
 export const getRows = (rows, period) => {
+  let { rowsPages, page } = rows[period];
 
-    let { rowsPages, page } = rows[period]
+  let row = rowsPages?.find((data) => {
+    return data?.hasOwnProperty(page);
+  });
 
-    let row = rowsPages?.find((data => {
-        return data?.hasOwnProperty(page)
-    }))
-
-    return row ? row[page] : []
-}
+  return row ? row[page] : [];
+};
 
 export const setPageByPeriod = (rows, page, period) => {
+  let ob = rows[period];
 
-    let ob = rows[period]
+  ob.page = page;
 
-    ob.page = page
-
-    return {
-        type: REDUX_ACTIONS.SET_PAGE_TO_ROWS_PERIOD,
-        payload: { ob, period }
-    }
-}
+  return {
+    type: REDUX_ACTIONS.SET_PAGE_TO_ROWS_PERIOD,
+    payload: { ob, period },
+  };
+};
 
 export const setSelected = (row) => {
-
-    return {
-        type: REDUX_ACTIONS.SET_SELECTED,
-        payload: row
-    }
-}
-
+  return {
+    type: REDUX_ACTIONS.SET_SELECTED,
+    payload: row,
+  };
+};
 
 export const setRowsSelected = (rows, period, selected) => {
+  let { page } = rows[period];
 
-    let { page } = rows[period]
+  let { lines = {} } = getRowsByPage(rows, period);
 
-    let { lines = {} } = getRowsByPage(rows, period)
+  // console.log({ page, lines });
 
-    console.log({ page, lines })
+  if (lines.hasOwnProperty(page)) {
+    lines[page].forEach((row, i) => {
+      selected.push({ ...row, index: i, page });
+    });
+  }
 
-    if (lines.hasOwnProperty(page)) {
+  // console.log({ selected });
 
-        console.log({ entries: lines[page] })
-
-        lines[page].forEach((row, i) => {
-            selected.push({ ...row, index: i, page })
-        });
-    }
-
-    console.log({ selected })
-
-    return {
-        type: REDUX_ACTIONS.UPDATE_SELECTED,
-        payload: selected
-    }
-}
+  return {
+    type: REDUX_ACTIONS.UPDATE_SELECTED,
+    payload: selected,
+  };
+};
 
 export const removeRowsSelected = (rows, period, selected) => {
+  let { page } = rows[period];
 
-    let { page } = rows[period]
+  let { lines = {} } = getRowsByPage(rows, period);
 
-    let { lines = {} } = getRowsByPage(rows, period)
+  if (lines.hasOwnProperty(page)) {
+    lines[page].forEach((row, i) => {
+      let indexed = selected.findIndex(
+        (row) => row?.page === page && row?.index === i
+      );
 
-    if (lines.hasOwnProperty(page)) {
+      if (indexed !== -1) {
+        selected.splice(indexed, 1);
+      }
+    });
+  }
 
-        lines[page].forEach((row, i) => {
-
-            let indexed = selected.findIndex((row) => (row?.page === page && row?.index === i))
-
-            if (indexed !== -1) {
-                selected.splice(indexed, 1)
-            }
-        });
-    }
-
-    return {
-        type: REDUX_ACTIONS.UPDATE_SELECTED,
-        payload: selected
-    }
-}
-
+  return {
+    type: REDUX_ACTIONS.UPDATE_SELECTED,
+    payload: selected,
+  };
+};
 
 export const removeSelected = (data) => {
+  let { page, index, selected } = data;
 
-    let { page, index, selected } = data
+  let indexed = selected.findIndex(
+    (row) => row?.page === page && row?.index === index
+  );
 
-    let indexed = selected.findIndex((row) => (row?.page === page && row?.index === index))
-
-    if (indexed !== -1) {
-        selected.splice(indexed, 1)
-    }
-    console.log({ newSelected: selected })
-    return {
-        type: REDUX_ACTIONS.UPDATE_SELECTED,
-        payload: selected
-    }
-}
+  if (indexed !== -1) {
+    selected.splice(indexed, 1);
+  }
+  // console.log({ newSelected: selected });
+  return {
+    type: REDUX_ACTIONS.UPDATE_SELECTED,
+    payload: selected,
+  };
+};
 
 export const getViewByPeriodAndPage = (rows, period) => {
+  let { rowsPages, page } = rows[period];
 
-    let { rowsPages, page } = rows[period]
+  let row = rowsPages?.find((data) => {
+    return data?.hasOwnProperty(page);
+  });
 
-    let row = rowsPages?.find((data => {
-        return data?.hasOwnProperty(page)
-    }))
-
-    return row ? row["views"] : null
-}
+  return row ? row["views"] : null;
+};
 
 export const getRowByPeriodAndPage = (rows, period, index) => {
+  let { rowsPages, page } = rows[period];
 
-    let { rowsPages, page } = rows[period]
+  let row = rowsPages?.find((data) => {
+    return data?.hasOwnProperty(page);
+  });
 
+  return row ? row[page][index] : null;
+};
 
-    let row = rowsPages?.find((data => {
-        return data?.hasOwnProperty(page)
-    }))
+export const getAllSelected = (rowsPages, page) => {
+  let index = rowsPages?.findIndex((data) => {
+    return data?.hasOwnProperty(page);
+  });
 
-    return row ? row[page][index] : null
-}
+  let row = rowsPages[index];
 
-export const getAllSelected=(rowsPages,page)=>{
-    let index = rowsPages?.findIndex((data => {
-        return data?.hasOwnProperty(page)
-    }))
+  console.log({ allSelection: row?.allSelection, page });
 
-    // let data=rowsPages[INDEX]
-    return index !== -1 ? {
-        index,
-        lines: rowsPages[index]
-    } : null
-}
+  return row?.allSelection ? true : false;
+};
 
 export const getRowsByPage = (rows, period) => {
+  let { rowsPages, page } = rows[period];
 
-    let { rowsPages, page } = rows[period]
+  let index = rowsPages?.findIndex((data) => {
+    return data?.hasOwnProperty(page);
+  });
 
-    let index = rowsPages?.findIndex((data => {
-        return data?.hasOwnProperty(page)
-    }))
-
-    return index !== -1 ? {
+  return index !== -1
+    ? {
         index,
-        lines: rowsPages[index]
-    } : null
-}
+        lines: rowsPages[index],
+      }
+    : null;
+};
 
 export const checkRow = (rowsPages, page, index) => {
+  let rowIndex = rowsPages?.findIndex((d) => {
+    return d?.hasOwnProperty(page);
+  });
 
-    let rowIndex = rowsPages?.findIndex((d) => {
-        return d?.hasOwnProperty(page)
-    })
+  if (rowIndex !== -1) {
+    let data = rowsPages[rowIndex];
 
-    if (rowIndex !== -1) {
-        let data = rowsPages[rowIndex]
+    data[page][index]["checked"] = !data[page][index]["checked"];
 
-        data[page][index]["checked"] = !data[page][index]["checked"]
+    rowsPages[rowIndex] = { ...data };
+  }
 
-        rowsPages[rowIndex] = { ...data }
-    }
-
-    return rowsPages
-}
+  return rowsPages;
+};
 
 export const checkRowsByPeriodAndPages = (rows, rowsPages, page, index) => {
+  let rowIndex = rowsPages?.findIndex((d) => {
+    return d?.hasOwnProperty(page);
+  });
 
-    let rowIndex = rowsPages?.findIndex((d) => {
-        return d?.hasOwnProperty(page)
-    })
+  if (rowIndex !== -1) {
+    let data = rowsPages[rowIndex];
 
-    if (rowIndex !== -1) {
-        let data = rowsPages[rowIndex]
+    data[page][index]["checked"] = !data[page][index]["checked"];
 
-        data[page][index]["checked"] = !data[page][index]["checked"]
+    rowsPages[rowIndex] = { ...data };
+  }
 
-        rowsPages[rowIndex] = { ...data }
-    }
-
-    return rowsPages
-}
+  return rowsPages;
+};
 
 export const updateRowByPeriodAndPage = (rows, period, index) => {
-    try {
-        let { rowsPages, page } = rows[period]
+  try {
+    let { rowsPages, page } = rows[period];
 
-        const data = checkRow(rowsPages, page, index)
+    const data = checkRow(rowsPages, page, index);
 
-        console.log({ data })
+    // console.log({ data });
 
-        rows[period].rowsPages = [...data]
+    rows[period].rowsPages = [...data];
 
-        return {
-            type: REDUX_ACTIONS.SET_UPDATED_ROWS,
-            payload: rows
-        }
-
-    } catch (error) {
-        console.log(error)
-    }
-}
+    return {
+      type: REDUX_ACTIONS.SET_UPDATED_ROWS,
+      payload: rows,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const updateRowsByPeriodAndPage = (rows, period, isChecked) => {
-    try {
+  try {
+    let { lines = {}, index } = getRowsByPage(rows, period);
 
-        let { lines = {}, index } = getRowsByPage(rows, period)
+    // console.log({ index });
 
-        console.log({ index })
+    let { page, rowsPages } = rows[period];
 
-        let { page, rowsPages } = rows[period]
+    if (lines.hasOwnProperty(page)) {
+      for (const row of lines[page]) {
+        row.checked = isChecked;
+      }
 
-        if (lines.hasOwnProperty(page)) {
+      rowsPages[index] = lines;
 
-            for (const row of lines[page]) {
-                row.checked = isChecked
-            }
-
-            rowsPages[index] = lines
-
-            rowsPages[index].allSelection = isChecked
-
-        }
-
-        rows[period].rowsPages = rowsPages
-
-
-        return {
-            type: REDUX_ACTIONS.SET_UPDATED_ROWS,
-            payload: rows
-        }
-
-    } catch (error) {
-        console.log(error)
+      rowsPages[index].allSelection = isChecked;
     }
-}
+
+    rows[period].rowsPages = rowsPages;
+
+    return {
+      type: REDUX_ACTIONS.SET_UPDATED_ROWS,
+      payload: rows,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/components/second/Second.js
+++ src/components/second/Second.js
@@ -1,59 +1,56 @@
-import React, { useEffect } from 'react'
-import SecondHeader from './SecondHeader';
-import axios from 'axios';
-import { DEFAULT_PERIOD, HOST } from '../../constants';
-import Head from '../table/Head';
-import { useDispatch, useSelector } from 'react-redux';
-import CaptionTable from '../table/CaptionTable';
-import Tbody from '../table/Tbody';
-import { setHeaders, setPages, setRows, setRowsPeerPages } from '../../actions/paginationActions';
+import React, { useEffect } from "react";
+import SecondHeader from "./SecondHeader";
+import axios from "axios";
+import { HOST } from "../../constants";
+import Head from "../table/Head";
+import { useDispatch, useSelector } from "react-redux";
+import CaptionTable from "../table/CaptionTable";
+import Tbody from "../table/Tbody";
+import { setHeaders, setPages, setRows } from "../../actions/paginationActions";
 
 function Second() {
+  const url = useSelector((state) => state.url);
 
-  const url = useSelector(state => state.url)
+  const dispatch = useDispatch();
 
-  const dispatch = useDispatch()
-
-  const { rows, period } = useSelector(state => state.pagination)
+  const { rows, period } = useSelector((state) => state.pagination);
 
   useEffect(() => {
-
     if (rows?.hasOwnProperty(period)) {
+      let rowsValue = rows[period];
 
-      let rowsValue = rows[period]
-
-      dispatch(setPages(rowsValue?.pages || 1))
+      dispatch(setPages(rowsValue?.pages || 1));
     }
-
   }, [rows, dispatch, period]);
 
   useEffect(() => {
     (async () => {
       try {
-        let { data } = await axios.get(`${HOST}${url}`)
-        dispatch(setRowsPeerPages({ rowsPage: data?.rows?.slice(0, DEFAULT_PERIOD) || [] }))
-        dispatch(setRows(data?.rows || []))
-        dispatch(setHeaders(data?.headers || []))
+        let { data } = await axios.get(`${HOST}${url}`);
+        // dispatch(setRowsPeerPages({ rowsPage: data?.rows?.slice(0, DEFAULT_PERIOD) || [] }))
+        dispatch(setRows(data?.rows || []));
+        dispatch(setHeaders(data?.headers || []));
       } catch (error) {
-        dispatch(setRows([]))
-        dispatch(setHeaders([]))
-        console.log(error)
+        dispatch(setRows([]));
+        dispatch(setHeaders([]));
+        console.log(error);
       }
-    })()
+    })();
   }, [url, dispatch]);
-
 
   return (
     <>
       <div className="second">
         <div className="second-body">
           <SecondHeader />
-          {Object.keys(rows)?.length ? <div className="panel-body bg-white table-responsive">
-            <table className="table table-bordered table-sm">
-              <Head />
-              <Tbody />
-            </table>
-          </div> : null}
+          {Object.keys(rows)?.length ? (
+            <div className="panel-body bg-white table-responsive">
+              <table className="table table-bordered table-sm">
+                <Head />
+                <Tbody />
+              </table>
+            </div>
+          ) : null}
           <CaptionTable />
         </div>
       </div>
@@ -61,4 +58,4 @@ function Second() {
   );
 }
 
-export default Second
+export default Second;
